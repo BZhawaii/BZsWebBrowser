46177aa914e07d31962810ab3cc2bc93b5f24146
Richard Beese <bzhawaii@yahoo.com>
2017-08-15 14:56:31 -0600

    Created file and about actions

--- BZsWebBrowser2.0/Form1.cs
+++ BZsWebBrowser2.0/Form1.cs
@@ -16,5 +16,20 @@ namespace BZsWebBrowser2._0
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// This function is called when the exit menu item is selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made by: BZ");
+        }
     }
 }
